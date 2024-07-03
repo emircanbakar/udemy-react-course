8db800c163a9551f2c87906138032404980b4bed
emircanbakar <emirbakar952@gmail.com>
2024-07-04 00:50:12 +0300

    küçük değişiklikler

--- B2/first-app/src/components/ProductsItem.jsx
+++ B2/first-app/src/components/ProductsItem.jsx
@@ -1,17 +1,21 @@
 import "./ProductItems.css"
 
 function ProductItem() {
+
+  const imageURL = "https://tarim.ibb.gov.tr/img/201681154251638537234067.jpg"
+  const productName = "Avokado"
+  const productPrice = "20"
   return (
     <div className="product-item">
       <div>
         <img
-          src="https://tarim.ibb.gov.tr/img/201681154251638537234067.jpg"
+          src= {imageURL}
           alt=""
         />
       </div>
       <div className="product-info">
-        <h2 className="product-title">Avokado</h2>
-        <span className="product-price">15 TL</span>
+        <h2 className="product-title"> {productName} </h2>
+        <span className="product-price"> {productPrice} TL</span>
       </div>
     </div>
   );
