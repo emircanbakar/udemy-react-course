bbe5789451ea3120fcef7683958ff61508845478
emircanbakar <emirbakar952@gmail.com>
2024-07-19 17:48:47 +0300

    state temizliği

--- B4/workers-app/src/components/Workers/AddWorker.jsx
+++ B4/workers-app/src/components/Workers/AddWorker.jsx
@@ -4,18 +4,27 @@ import Button from "../ui/Button";
 
 function AddWorker() {
   const [employeeName, setEmployeeName] = useState("");
-  const [wage, setWage] = useState("")
-  const employeeChangeHandler = (e) =>{
-    setEmployeeName(e.target.value)
-  }
-  const wageChangeHandler = (e) =>{
-    setWage(e.target.value)
-  }
+  const [wage, setWage] = useState("");
+  const minWage = 5000;
+  const employeeChangeHandler = (e) => {
+    setEmployeeName(e.target.value);
+  };
+  const wageChangeHandler = (e) => {
+    setWage(e.target.value);
+  };
 
-  const addWorkerHandler = (e) =>{
-    e.preventDefault()
-    console.log(employeeName, wage)
-  }
+  const addWorkerHandler = (e) => {
+    e.preventDefault();
+    if (employeeName.trim().length === 0 || wage.trim().length === 0) {
+      return;
+    }
+    if (+wage < minWage) {
+      return;
+    }
+    console.log(employeeName, wage);
+    setEmployeeName("");
+    setWage("");
+  };
   return (
     <Card className="mt-8">
       <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
@@ -28,6 +37,7 @@ function AddWorker() {
           className="max-w-[40rem] border mx-auto w-full p-2 "
           placeholder="Çalışan ismi giriniz."
           onChange={employeeChangeHandler}
+          value={employeeName}
         />
         <label htmlFor="wage" className="font-medium">
           Maaş Miktari
@@ -38,8 +48,11 @@ function AddWorker() {
           id="wage"
           placeholder="Maaş miktari giriniz."
           onChange={wageChangeHandler}
+          value={wage}
         />
-        <Button type="submit" className="mt-2">Ekle</Button>
+        <Button type="submit" className="mt-2">
+          Ekle
+        </Button>
       </form>
     </Card>
   );
