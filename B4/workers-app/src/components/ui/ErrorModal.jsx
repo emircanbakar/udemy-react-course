f2a64087762d4bf194db57753be0f97ad42ea7d0
emircanbakar <emirbakar952@gmail.com>
2024-07-26 00:15:05 +0300

    portals

--- B4/workers-app/src/components/ui/ErrorModal.jsx
+++ B4/workers-app/src/components/ui/ErrorModal.jsx
@@ -1,28 +1,49 @@
 import React from "react";
 import Button from "./Button";
 import Card from "./Card";
+import ReactDOM from "react-dom";
 
-const ErrorModal = (props) => {
-    const {onConfirm, error} = props;
-    const {message, title} = error;
-    return (
+const Backdrop = (props) => {
+  return (
+    <div
+      className="backdrop-blur-sm  bg-white/30 fixed w-screen h-screen top-0 left-0"
+      onClick={props.onConfirm}
+    ></div>
+  );
+};
+
+const Overlay = (props) => {
+  return (
     <div className="error-modal ">
-      <div
-        className="backdrop-blur-sm  bg-white/30 fixed w-screen h-screen top-0"
-        onClick={onConfirm}
-      ></div>
       <Card className="w-[36rem] p-0 z-20">
         <header className="bg-red-700 p-4 text-white rounded-t-xl">
-          <h2 className="text-center text-xl"> {title} </h2>
+          <h2 className="text-center text-xl"> {props.title} </h2>
         </header>
-        <section className="p-4"> {message} </section>
+        <section className="p-4"> {props.message} </section>
         <footer className="p-4 flex justify-end">
-          <Button onClick={onConfirm} className="w-32">
+          <Button onClick={props.onConfirm} className="w-32">
             Tamam
           </Button>
         </footer>
       </Card>
     </div>
+  );
+};
+
+const ErrorModal = (props) => {
+  const { onConfirm, error } = props;
+  const { message, title } = error;
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Overlay onConfirm={onConfirm} title={title} message={message} />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 };
 
