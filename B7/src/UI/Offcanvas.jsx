367b89ff3e1078195c9ccc384c4cebfbe843b9e1
emircanbakar <emirbakar952@gmail.com>
2024-09-20 02:37:56 +0300

    portal eklene

--- B7/src/UI/Offcanvas.jsx
+++ B7/src/UI/Offcanvas.jsx
@@ -1,12 +1,30 @@
-import "./Offcanvas.css"
+import "./Offcanvas.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.hideCart}></div>;
+};
+
+const OffcanvasOverlay = (props) => {
+  return (
+    <div className="offcanvas">
+      <div className="content"> {props.children} </div>
+    </div>
+  );
+};
 
 const Offcanvas = (props) => {
+  const portalElement = document.getElementById("overlays");
   return (
     <>
-      <div className="backdrop" onClick={props.hideCart}></div>
-      <div className="offcanvas">
-        <div className="content"> {props.children} </div>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop hideCart={props.hideCart} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <OffcanvasOverlay> {props.children} </OffcanvasOverlay>,
+        portalElement
+      )}
     </>
   );
 };
